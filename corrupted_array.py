e489367b84c80644b6ae2b3f4be21bd7826a43da
khan-walker <khanwalkergithub@gmail.com>
2024-05-15 12:12:50 +0500

    corrected version

--- corrupted_array.py
+++ corrupted_array.py
@@ -1,33 +1,11 @@
-t = int(input())
-
-for _ in range(t):
+for _ in [0] * int(input()):
     n = int(input())
-    b = list(map(int, input().split()))
-    b.sort()
-    if n < 3:
-        print(-1)
-        continue        
-    for i in [1, 2, 3]:
-        maxi = b[-i]
-        over = False
-        summa = sum(b[:-i])
-        if i == 1:
-            for j in range(0, n - 1):
-                if summa - b[j] == maxi:
-                    print(*(b[:j] + b[j + 1:-1]))
-                    over = True
-                    break
-        elif i == 2:
-            if summa == b[-2]:
-                print(*b[:-2])
-                break
-        elif i == 3:
-            print(-1)
-            break
-        if over:
-            break
+    a = sorted(map(int, input().split()))
+    s = sum(a)
+    if s - a[-1] == 2 * a[-2]: print(*a[:-2]); continue
+    if s - 2 * a[-1] in a[:-1]:
+        m = a[:-1].index(s - 2 * a[-1])
+        print(*[a[i] for i in range(n + 1) if i != m])
+        continue
 
-
-
-
-            
+    print(-1)
